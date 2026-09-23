e4ab65882fe9bacdb035bd19400d30cd38c43b18
siva <23ht1a4374@gmail.com>
2026-09-23 23:56:20 +0530

    Feat : security increased

--- Server/controlers/auth.controler.js
+++ Server/controlers/auth.controler.js
@@ -17,8 +17,8 @@ export async function googleAuth(req,res){
         },process.env.JWT_SECRET,{expiresIn: "7d"});
        res.cookie("token",token,{
         httpOnly: true,
-        secure :false,
-        sameSite: "strict",
+        secure :true,
+        sameSite: "none",
          maxAge: 7 * 24 * 60 * 60 * 1000
        })
     return res.status(200).json({user: currentUser, token});
@@ -32,8 +32,8 @@ export async function logOut(req,res){
     try{
         return res.clearCookie("token",{
         httpOnly: true,
-        secure :false,
-        sameSite: "strict",
+        secure :true,
+        sameSite: "none",
 
         }).status(200).json({message: "Logout successful"})
     }
